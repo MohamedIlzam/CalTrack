1fd7b1757e3c6e0547ac2b526c2b3a29f91721ae
MohamedIlzam <it24610792@my.sliit.lk>
2026-06-02 23:56:03 +0530

    fix: resolve backend compilation and configure gitignore

--- backend/dist/generated/prisma/browser.d.ts
+++
@@ -1,8 +0,0 @@
-import * as Prisma from './internal/prismaNamespaceBrowser.js';
-export { Prisma };
-export * as $Enums from './enums.js';
-export * from './enums.js';
-export type User = Prisma.UserModel;
-export type FoodItem = Prisma.FoodItemModel;
-export type MealLog = Prisma.MealLogModel;
-export type MealLogEntry = Prisma.MealLogEntryModel;
--- backend/dist/generated/prisma/models.d.ts
+++
@@ -1,5 +0,0 @@
-export type * from './models/User.js';
-export type * from './models/FoodItem.js';
-export type * from './models/MealLog.js';
-export type * from './models/MealLogEntry.js';
-export type * from './commonInputTypes.js';
